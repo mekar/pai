304d498adeae21daf7dd966ef3082f5daa958720
Rahul <trahul023@gmail.com>
2013-09-04 20:23:10 +0530

    Creating rails app with postgresql

---
+++ pain/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pain::Application.config.secret_token = '5b5d7bc2dec2db7123194e88fea8fc2d7d92971bc7eb02800a79352b4f62ce6c310eaac32d8f65b0bfaf42f3a49e7ee2dc7f34cf211824ec04a548b4370317c6'
